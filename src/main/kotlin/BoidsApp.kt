835044e50386b42cb17cbf81ffa8a965afae955f
PotatoPresident <51728317+PotatoPresident@users.noreply.github.com>
2021-11-14 15:12:00 -0800

    Rewrite to use velocity

--- src/main/kotlin/BoidsApp.kt
+++ src/main/kotlin/BoidsApp.kt
@@ -1,5 +1,4 @@
 import io.nacular.doodle.application.Application
-import io.nacular.doodle.controls.Photo
 import io.nacular.doodle.controls.range.Slider
 import io.nacular.doodle.core.Display
 import io.nacular.doodle.core.plusAssign
@@ -8,6 +7,7 @@ import io.nacular.doodle.drawing.Canvas
 import io.nacular.doodle.drawing.Color
 import io.nacular.doodle.drawing.Stroke
 import io.nacular.doodle.drawing.paint
+import io.nacular.doodle.event.PointerMotionListener
 import io.nacular.doodle.geometry.ConvexPolygon
 import io.nacular.doodle.geometry.Point
 import io.nacular.doodle.geometry.Size
@@ -20,12 +20,15 @@ import io.nacular.measured.units.Angle
 import io.nacular.measured.units.Measure
 import io.nacular.measured.units.Time
 import io.nacular.measured.units.times
-import kotlinx.coroutines.*
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlin.math.atan2
-import kotlin.math.cos
-import kotlin.math.sin
+import kotlin.math.pow
+import kotlin.math.sqrt
+import kotlin.random.Random
 
-var separation = 1.0
+var separation = 1.5
 var alignment = 1.0
 var cohesion = 1.0
 
@@ -41,7 +44,7 @@ class BoidsApp(
 
     init {
         GlobalScope.launch {
-            fish = imageLoader.load("")
+            fish = imageLoader.load("fish.png")
         }
 
         themes.selected = theme
@@ -71,11 +74,15 @@ class BoidsApp(
                 changed += { source, old, new -> cohesion = new }
             }
 
-            for (i in 1..40) {
+            for (i in 1..150) {
                 flock += Boid(
-                    Point((0..size.width.toInt()).random(), (0..size.height.toInt()).random()),
-                    (0..360).random() * Angle.degrees
+                    Point((0..size.width.toInt()).random(), (0..size.height.toInt()).random())
                 )
+            }
+
+            var pointerPos: Point? = null
+            pointerMotionChanged += PointerMotionListener.moved { event ->
+                pointerPos = event.location
             }
 
             render = {
@@ -85,6 +92,7 @@ class BoidsApp(
 
                 for (boid in flock) {
                     boid.applyRules(flock, this)
+                    //pointerPos?.let { boid.addForce(boid.target(it) * 0.8) }
                     boid.move(this)
                     boid.render(this)
                 }
@@ -97,18 +105,21 @@ class BoidsApp(
 
 data class Boid(
     var pos: Point = Point.Origin,
-    var rotation: Measure<Angle> = 0 * Angle.degrees,
-    val speed: Double = 1.0,
+    var velocity: Point = Point(Random.nextDouble(-1.0, 1.0), Random.nextDouble(-1.0, 1.0)),
+    var acceleration: Point = Point(0, 0),
     val viewRadius: Int = 200,
-    val maxTurnForce: Double = 1.0,
+    val maxSpeed: Double = 3.0,
+    val maxSteerForce: Double = 0.05,
     val color: Color = Color.Blue,
 ) {
+
     fun render(canvas: Canvas) {
-        canvas.rotate(pos + Point(10, 10), rotation) {
+        canvas.rotate(pos + Point(10, 10), velocity.heading * Angle.radians) {
             canvas.translate(pos) {
                 canvas.scale(10.0, 10.0) {
                     val poly = ConvexPolygon(Point(0, 2), Point(0, 0), Point(3, 1))
                     poly(poly, color.paint)
+
                     /*
                     fish?.let {
                         image(it, pos, 1f, 0.0)
@@ -119,43 +130,48 @@ data class Boid(
         }
     }
 
-    val velocity: Point
-        get() = Point(
-            speed * cos((rotation `as` Angle.radians).amount),
-            speed * sin((rotation `as` Angle.radians).amount)
-        )
+    fun addForce(force: Point) {
+        acceleration += force
+    }
 
     fun move(canvas: Canvas) {
+        velocity += acceleration
+        velocity = velocity.limit(maxSpeed)
+
         pos += velocity
 
+        acceleration = Point(0, 0)
+
+        // Wrap around edges
         val x = pos.x fmod canvas.size.width
         val y = pos.y fmod canvas.size.height
 
         pos = Point(x, y)
     }
 
-    private fun align(flock: Set<Boid>): Measure<Angle> {
-        var turnForce = 0.0
+    private fun align(flock: Set<Boid>): Point {
+        var avg = Point(0, 0)
         var total = 0
         for (boid in flock) {
             val distance = this.pos.distanceFrom(boid.pos)
             if (boid !== this && distance < viewRadius) {
-                turnForce += boid.rotation.amount
+                avg += boid.velocity
                 total++
             }
         }
-        if (total > 0) {
-            turnForce /= total
-            turnForce -= this.rotation.amount
-            turnForce = turnForce.coerceIn(-maxTurnForce, maxTurnForce)
-        }
+        return if (total > 0) {
+            avg /= total
+            avg = avg.normalize()
+            avg *= this.maxSpeed
 
-        return turnForce * Angle.degrees
+            var steerForce = avg - this.velocity
+            steerForce = steerForce.limit(maxSteerForce)
+            steerForce
+        } else Point(0, 0)
     }
 
-    private fun cohesion(flock: Set<Boid>, canvas: Canvas): Measure<Angle> {
-        var turnForce = 0.0
-        var avgPos = Point.Origin
+    private fun cohesion(flock: Set<Boid>, canvas: Canvas): Point {
+        var avgPos = Point(0, 0)
         var total = 0
         for (boid in flock) {
             val distance = this.pos.distanceFrom(boid.pos)
@@ -164,50 +180,78 @@ data class Boid(
                 total++
             }
         }
-        if (total > 0) {
+
+        return if (total > 0) {
             avgPos /= total
-            val targetVec = avgPos - this.pos
-            val targetRot = (atan2(targetVec.y, targetVec.x)) * Angle.radians
 
-            turnForce = (targetRot `as` Angle.degrees).amount
-            turnForce -= this.rotation.amount
-            turnForce = turnForce.coerceIn(-maxTurnForce * cohesion, maxTurnForce * cohesion)
-
-            canvas.line(this.pos, avgPos, Stroke(Color.Red))
-        }
-
-        return turnForce * Angle.degrees
+            target(avgPos)
+        } else Point(0, 0)
     }
 
-    private fun separation(flock: Set<Boid>): Measure<Angle> {
-        var turnForce = 0.0
+    private fun separation(flock: Set<Boid>): Point {
+        val targetSep = 100.0
+        var steerForce = Point.Origin
         var total = 0
         for (boid in flock) {
             val distance = this.pos.distanceFrom(boid.pos)
-            if (boid !== this && distance < viewRadius) {
+            if (boid !== this && distance < targetSep) {
                 var diff = this.pos - boid.pos
-                diff /= distance
-                turnForce += (((atan2(diff.y, diff.x)) * Angle.radians) `as` Angle.degrees).amount
+                diff = diff.normalize()
+                diff /= (distance)
+                steerForce += diff
 
                 total++
             }
         }
         if (total > 0) {
-            turnForce /= total
-            turnForce -= this.rotation.amount
-            turnForce = turnForce.coerceIn(-maxTurnForce, maxTurnForce)
+            steerForce /= total
         }
 
-        return turnForce * Angle.degrees
+        if (steerForce.mag() > 0.0) {
+            steerForce = steerForce.normalize()
+            steerForce *= this.maxSpeed
+            steerForce -= this.velocity
+            steerForce = steerForce.limit(this.maxSteerForce)
+        }
+
+        return steerForce
+    }
+
+    fun target(point: Point): Point {
+        var targetVec = point - this.pos
+        targetVec = targetVec.normalize()
+        targetVec *= maxSpeed
+
+        val steerForce = targetVec - this.velocity
+        return steerForce.limit(maxSteerForce)
     }
 
     fun applyRules(flock: Set<Boid>, canvas: Canvas) {
-        this.rotation += align(flock) * alignment
-        //this.rotation += cohesion(flock, canvas)
-        //this.rotation += separation(flock) * separation
+        addForce(separation(flock) * separation)
+        addForce(align(flock) * alignment)
+        addForce(cohesion(flock, canvas) * cohesion)
     }
 }
 
 infix fun Double.fmod(other: Double): Double {
     return ((this % other) + other) % other
 }
+
+fun Point.normalize(): Point {
+    val l = 1.0 / sqrt(x * x + y * y)
+    return Point(x * l, y * l)
+}
+
+fun Point.mag() = sqrt(x.pow(2) + y.pow(2))
+fun Point.magSq() = x.pow(2) + y.pow(2)
+
+fun Point.limit(max: Double): Point {
+    if (magSq() > max.pow(2)) {
+        val norm = normalize()
+        return norm * max
+    }
+
+    return this
+}
+
+val Point.heading get() = atan2(y, x)
