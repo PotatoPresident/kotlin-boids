758e50d0febbfaf665672e2265452b61217491c9
PotatoPresident <51728317+PotatoPresident@users.noreply.github.com>
2021-11-14 16:15:59 -0800

    Made prettier

---
+++ src/main/kotlin/Boid.kt
@@ -0,0 +1,167 @@
+import io.nacular.doodle.drawing.Canvas
+import io.nacular.doodle.drawing.Color
+import io.nacular.doodle.drawing.lighter
+import io.nacular.doodle.drawing.paint
+import io.nacular.doodle.geometry.ConvexPolygon
+import io.nacular.doodle.geometry.Point
+import io.nacular.measured.units.Angle
+import io.nacular.measured.units.times
+import kotlin.math.atan2
+import kotlin.math.pow
+import kotlin.math.sqrt
+import kotlin.random.Random
+
+private val colors = arrayListOf(Color.Green.lighter(0.3f), Color.Blue.lighter(0.3f), Color.Cyan.lighter(0.2f))
+
+data class Boid(
+    var pos: Point = Point.Origin,
+    var velocity: Point = Point(Random.nextDouble(-1.0, 1.0), Random.nextDouble(-1.0, 1.0)),
+    var acceleration: Point = Point(0, 0),
+    val viewRadius: Int = 200,
+    val maxSpeed: Double = 3.0,
+    val maxSteerForce: Double = 0.05,
+    val color: Color = colors.random(),
+) {
+
+    fun render(canvas: Canvas) {
+        canvas.rotate(pos + Point(10, 10), velocity.heading * Angle.radians) {
+            canvas.translate(pos) {
+                canvas.scale(10.0, 10.0) {
+                    val poly = ConvexPolygon(Point(0, 2), Point(0, 0), Point(3, 1))
+                    poly(poly, color.paint)
+
+                    /*
+                    fish?.let {
+                        image(it, pos, 1f, 0.0)
+                    }
+                     */
+                }
+            }
+        }
+    }
+
+    fun addForce(force: Point) {
+        acceleration += force
+    }
+
+    fun move(canvas: Canvas) {
+        velocity += acceleration
+        velocity = velocity.limit(maxSpeed)
+
+        pos += velocity
+
+        acceleration = Point(0, 0)
+
+        // Wrap around edges
+        val x = pos.x fmod canvas.size.width
+        val y = pos.y fmod canvas.size.height
+
+        pos = Point(x, y)
+    }
+
+    private fun align(flock: Set<Boid>): Point {
+        var avg = Point(0, 0)
+        var total = 0
+        for (boid in flock) {
+            val distance = this.pos.distanceFrom(boid.pos)
+            if (boid !== this && distance < viewRadius) {
+                avg += boid.velocity
+                total++
+            }
+        }
+        return if (total > 0) {
+            avg /= total
+            avg = avg.normalize()
+            avg *= this.maxSpeed
+
+            var steerForce = avg - this.velocity
+            steerForce = steerForce.limit(maxSteerForce)
+            steerForce
+        } else Point(0, 0)
+    }
+
+    private fun cohesion(flock: Set<Boid>): Point {
+        var avgPos = Point(0, 0)
+        var total = 0
+        for (boid in flock) {
+            val distance = this.pos.distanceFrom(boid.pos)
+            if (boid !== this && distance < viewRadius) {
+                avgPos += boid.pos
+                total++
+            }
+        }
+
+        return if (total > 0) {
+            avgPos /= total
+
+            target(avgPos)
+        } else Point(0, 0)
+    }
+
+    private fun separation(flock: Set<Boid>): Point {
+        val targetSep = 100.0
+        var steerForce = Point.Origin
+        var total = 0
+        for (boid in flock) {
+            val distance = this.pos.distanceFrom(boid.pos)
+            if (boid !== this && distance < targetSep) {
+                var diff = this.pos - boid.pos
+                diff = diff.normalize()
+                diff /= (distance)
+                steerForce += diff
+
+                total++
+            }
+        }
+        if (total > 0) {
+            steerForce /= total
+        }
+
+        if (steerForce.mag() > 0.0) {
+            steerForce = steerForce.normalize()
+            steerForce *= this.maxSpeed
+            steerForce -= this.velocity
+            steerForce = steerForce.limit(this.maxSteerForce)
+        }
+
+        return steerForce
+    }
+
+    fun target(point: Point): Point {
+        var targetVec = point - this.pos
+        targetVec = targetVec.normalize()
+        targetVec *= maxSpeed
+
+        val steerForce = targetVec - this.velocity
+        return steerForce.limit(maxSteerForce)
+    }
+
+    fun applyRules(flock: Set<Boid>) {
+        addForce(separation(flock) * separation)
+        addForce(align(flock) * alignment)
+        addForce(cohesion(flock) * cohesion)
+    }
+}
+
+infix fun Double.fmod(other: Double): Double {
+    return ((this % other) + other) % other
+}
+
+fun Point.normalize(): Point {
+    val l = 1.0 / sqrt(x * x + y * y)
+    return Point(x * l, y * l)
+}
+
+fun Point.mag() = sqrt(x.pow(2) + y.pow(2))
+fun Point.magSq() = x.pow(2) + y.pow(2)
+
+fun Point.limit(max: Double): Point {
+    if (magSq() > max.pow(2)) {
+        val norm = normalize()
+        return norm * max
+    }
+
+    return this
+}
+
+val Point.heading get() = atan2(y, x)
